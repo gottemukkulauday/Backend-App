9afd2762b7be70c17020457fcab37656cdb41d7c
gottemukkulauday <udaygottemukkulla59@gmail.com>
2026-03-13 11:56:00 +0530

    modified:   controllers/authController.js 	new file:   controllers/orderController.js 	modified:   index.js 	modified:   middleware/auth.js 	new file:   models/orderModel.js 	modified:   package-lock.json 	modified:   package.json 	new file:   public/images/9.png 	modified:   routes/authRoute.js 	new file:   routes/orderRoute.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,21 @@
 import userModel from "../models/userModel.js";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+const SECRET = "mysecret"
 const login = async (req, res) => {
   res.render("auth/login");
 };
 const validateUser = async (req, res) => {
   const { email, password } = req.body;
-  const user = await userModel.findOne({ email });          // ← no role filter
-  if (user && await bcrypt.compare(password, user.password)) {
-    req.session.user = user;
-    res.redirect("/");
+  const user = await userModel.findOne({ email, role: "admin" });
+  if (user) {
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      req.session.user = user;
+      res.redirect("/");
+    } else {
+      res.redirect("/auth/login");
+    }
   } else {
     res.redirect("/auth/login");
   }
@@ -18,18 +25,44 @@ const register = async (req, res) => {
 };
 
 const registerUser = async (req, res) => {
-  const { name, email, password, role } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  await userModel.create({ name, email, password: hashedPassword, role });
+  const body = req.body;
+  const hashedPassword = await bcrypt.hash(body.password, 10);
+  body.password = hashedPassword;
+  await userModel.create(body);
   res.redirect("/auth/login");
 };
 
 const signup = async (req, res) => {
   const { name, email, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
-  password = hashedPassword;
-  await userModel.create({ name, email, password: hashedPassword });
-  res.json({ message: "User Created" });
+  // password = hashedPassword;
+  const response = await userModel.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+  res.json(response);
+};
+
+const signin = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await userModel.findOne({ email });
+  if (user) {
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      const userObj = {
+        name:user.name,
+        email:user.email,
+        role:user.role,
+      };
+      const token = jwt.sign(userObj, SECRET, { expiresIn: "1h" });
+      res.json({ ...userObj, token });
+    } else {
+      res.json({ error: "Invalid Password" });
+    }
+  } else {
+    res.json({ error: "Invalid User" });
+  }
 };
 
 const logout = (req, res) => {
@@ -38,4 +71,4 @@ const logout = (req, res) => {
   res.render("auth/login");
 };
 
-export { login, validateUser, register, registerUser, logout };
+export { login, validateUser, register, registerUser, logout, signup, signin };
